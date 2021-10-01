7ab5bbb7e7e5390bb64a2ac4e9d24b2177bcf942
hardikbachhan <50964127+hardikbachhan@users.noreply.github.com>
2021-10-01 18:39:30 +0530

    fix alert height(cle problem)

--- src/components/Alert.js
+++ src/components/Alert.js
@@ -1,25 +1,28 @@
 import React from "react";
 
 function Alert(props) {
-
-    const capitalize = (message) => {
-        const lower = message.toLowerCase();
-        return lower.charAt(0).toUpperCase() + lower.slice(1);
-    };
+  const capitalize = (message) => {
+    const lower = message.toLowerCase();
+    return lower.charAt(0).toUpperCase() + lower.slice(1);
+  };
 
   return (
-    props.alert && (<div
-      className={`alert alert-${props.alert.type} alert-dismissible fade show`}
-      role="alert"
-    >
-      {capitalize(props.alert.type)}: <strong>{props.alert.message}</strong>
-      {/* <button
+    <div style={{ height: "50px" }}>
+      {props.alert && (
+        <div
+          className={`alert alert-${props.alert.type} alert-dismissible fade show`}
+          role="alert"
+        >
+          {capitalize(props.alert.type)}: <strong>{props.alert.message}</strong>
+          {/* <button
         type="button"
         className="btn-close"
         data-bs-dismiss="alert"
         aria-label="Close"
       ></button> */}
-    </div>)
+        </div>
+      )}
+    </div>
   );
 }
 
